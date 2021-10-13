5e1d8650c5ab271792e13d1cd3445fb127f61aec
蒋金 <ginlink@163.com>
2021-10-13 19:36:39 +0800

    fix: 修复默认学生中心页面时，无法返回应用中心的问题

---
+++ pages/record/index.vue
@@ -0,0 +1,26 @@
+<!--
+ * @Author: jiangjin
+ * @Date: 2021-09-30 16:55:30
+ * @LastEditTime: 2021-10-13 17:58:35
+ * @LastEditors: jiangjin
+ * @Description: 
+ * 
+-->
+<template>
+  <view class="record"> </view>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {}
+  },
+  created() {},
+  methods: {},
+}
+</script>
+<style lang="scss" scoped>
+.record {
+}
+</style>
