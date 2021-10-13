2a4d7dcafaf9fa9289165377997af21d22973679
蒋金 <ginlink@163.com>
2021-10-13 20:50:34 +0800

    feat: 增加更新日志

--- common/misc.js
+++ common/misc.js
@@ -1,7 +1,7 @@
 /*
  * @Author: jiangjin
  * @Date: 2021-10-13 19:20:53
- * @LastEditTime: 2021-10-13 19:56:30
+ * @LastEditTime: 2021-10-13 20:50:03
  * @LastEditors: jiangjin
  * @Description:
  *
@@ -10,6 +10,8 @@
 export const START_PAGE = 'START_PAGE'
 
 export const VERSION_NODES = [
-  { version: '1.0.0', pieces: ['* 更新一条', '* 更新第二条'], updateTime: '2021.08.08 14:00:00' },
-  { version: '1.0.1', pieces: ['* 更新一条', '* 更新第二条'], updateTime: '2021.08.08 14:00:00' },
+  { version: '1.0.0', pieces: ['* 增加请假、销假、续假功能'], updateTime: '2021.10.12 00:29:52' },
+  {
+    version: '1.1.0', pieces: ['* 增加设置、更新日志页面', '* 样式：调整学生中心home图标、修改所有标题居中、调整假条详情样式'], updateTime: '2021-10-13 20:48:45'
+  },
 ]
